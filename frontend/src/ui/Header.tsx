a19c50972a5d3a06c327b1fd2a69dac90ebbb99e
Mohammed Akinbayo <mo@Mohammeds-MacBook-Pro.local>
2024-09-14 15:52:05 -0400

    Finished landing page

---
+++ frontend/src/ui/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return (
+        <div className="border-b-4 border-primary shadow-lg text-center">
+            <p className="py-2 text-primary">Logo</p>
+        </div>
+    )
+}
