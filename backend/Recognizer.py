a3dd06199ccb10c61785938edff56cb6f5504775
adityar21 <adityar21@vt.edu>
2024-09-14 20:48:37 -0400

    fix after git issues

--- backend/Recognizer.py
+++ backend/Recognizer.py
@@ -4,7 +4,6 @@ import numpy as np
 import torch
 from transformers import DetrImageProcessor, DetrForObjectDetection
 from PIL import ImageFilter
-
 import replicate
 import base64
 
@@ -13,34 +12,40 @@ file_name = 'object_mask.png'
 image_url = "https://www.furniturestorelosangeles.com/media/catalog/product/cache/b9a5bb227f7b0b98d739db40c623248a/n/o/norwood-rustic-grey-bedroom-set.jpg"
 
 # Load the image
-url = "https://www.furniturestorelosangeles.com/media/catalog/product/cache/b9a5bb227f7b0b98d739db40c623248a/n/o/norwood-rustic-grey-bedroom-set.jpg"
-image = Image.open(requests.get(url, stream=True).raw)
+def load_image(url):
+    try: 
+        return Image.open(requests.get(url, stream=True).raw)
+    except Exception as e:
+        print("There was an error loading your image")
+        return None
+    
 
-# Load object detection model and processor
-processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50")
-detection_model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50")
+def perform_detection(image):
+    # Load object detection model and processor
+    processor = DetrImageProcessor.from_pretrained("facebook/detr-resnet-50")
+    detection_model = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50")
+    # Perform object detection
+    inputs = processor(images=image, return_tensors="pt")
+    outputs = detection_model(**inputs)
+    target_sizes = torch.tensor([image.size[::-1]])
+    results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.70)[0]  # Lowered threshold
+    return results
 
-# Perform object detection
-inputs = processor(images=image, return_tensors="pt")
-outputs = detection_model(**inputs)
-target_sizes = torch.tensor([image.size[::-1]])
-results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.70)[0]  # Lowered threshold
+def create_mask(image, results):
+    # Create the mask
+    mask = Image.new('L', image.size, 0)  # 'L' mode for grayscale
+    draw = ImageDraw.Draw(mask)
+    # Draw bounding boxes on the mask
+    for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+        box = [int(i) for i in box]
+        print(f"Drawing box with coordinates: {box}")  # Debugging print
+        draw.rectangle(box, outline=255, fill=255)  # Draw white rectangles
 
-# Create the mask
-mask = Image.new('L', image.size, 0)  # 'L' mode for grayscale
-draw = ImageDraw.Draw(mask)
-
-# Draw bounding boxes on the mask
-for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
-    box = [int(i) for i in box]
-    print(f"Drawing box with coordinates: {box}")  # Debugging print
-    draw.rectangle(box, outline=255, fill=255)  # Draw white rectangles
-
-mask = mask.filter(ImageFilter.GaussianBlur(radius=2))
-mask.show()  # Show the mask to verify
-
+    mask = mask.filter(ImageFilter.GaussianBlur(radius=2))
+    mask.show()  # Show the mask to verify
+    mask.save("object_mask.png")  
+    return mask
 # Save the mask
-mask.save("object_mask.png")  #THIS IMAGE NEEDS TO BE SAVED IN S3 BUCKET
 
 def create_data_uri(file_path):
     with open(file_path, "rb") as file:
@@ -52,7 +57,6 @@ def create_data_uri(file_path):
         data_uri = f"data:application/octet-stream;base64,{encoded_data}"
         return data_uri
 
-
 def run_remover(mask_uri):
     output = replicate.run(
         "zylim0702/remove-object:0e3a841c913f597c1e4c321560aa69e2bc1f15c65f8c366caafc379240efd8ba",
@@ -62,7 +66,6 @@ def run_remover(mask_uri):
         }
     )
     print(output)
-
 
 def check_url_validity(url):
     response = requests.get(url)
@@ -80,3 +83,4 @@ def main():
     
 if __name__ == "__main__":
     main()
+
